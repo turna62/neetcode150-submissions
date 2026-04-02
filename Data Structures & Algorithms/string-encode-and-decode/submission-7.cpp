963507d85cb0bd5d27cfc97a9c692daf6434f15a
Lamia Alam Turna <95170568+turna62@users.noreply.github.com>
2026-04-02 16:34:30 +0600

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string enc = "";
+        for(string x: strs){
+            enc += x;
+            enc += "]";
+        }
+        return enc;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> result;
+        string p = "";
+        for(int i=0; i< s.size(); i++ ){
+            if(s[i] != ']'){
+                p += s[i];
+            }
+            else{
+                result.push_back(p);
+                p ="";
+            }
+        }
+        return result;
+    }
+};
